7f2f2fbad41af84c603c348b90aa389b6ecba3ec
Thomas Raout <thomraout@gmail.com>
2024-11-19 09:22:23 +0100

    added dummy fiber route

--- app/api/src/routes/routes.go
+++ app/api/src/routes/routes.go
@@ -2,9 +2,25 @@ package routes
 
 import (
 	"gobby/src/logger"
+	"time"
+
+	"github.com/gofiber/fiber/v3"
 )
 
 func InitRoutes() {
 	logger.Sugar.Info("initializing routes")
-	// TODO implement routes init with fiber
+	app := fiber.New()
+
+	// TODO assert parallelism behavior
+	app.Get("/", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		logger.Sugar.Info("Hello, wait 10s")
+		time.Sleep(8 * time.Second)
+		logger.Sugar.Info("Bye")
+		return nil
+	})
+
+	// Start the server on port 3000
+	logger.Sugar.Info("now listening on port 3000")
+	app.Listen(":3000")
 }
